46dff432c17ca4b4b02963e8383141f70876cb89
Mark Nardi <nardimark7@gmail.com>
2023-12-03 20:07:50 +0100

    build dist with static files

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         sveltekit(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: path.resolve(__dirname, './static') + '/[!.]*', // 1️⃣
+                    dest: './', // 2️⃣
+                },
+            ],
+        }),
     ],
     server: {
         fs: {

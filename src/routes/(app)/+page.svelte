402f3dafc289022f7190f261780270dc0a0e40e2
Max Rialto <maxrialto98@gmail.com>
2023-12-22 08:47:03 +0100

    added create game endpoint and form

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,1 +1,40 @@
-<h1>Let's f goooo</h1>
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import { fetchApi } from '$lib/utils/api';
+
+    const gameData = {
+        columns: 9,
+        rows: 9,
+    };
+
+    async function createGame() {
+        const { gameId } = await fetchApi('/api/create-game', 'POST', gameData);
+        goto(`/play/${gameId}`);
+    }
+</script>
+
+<game-settings class="p-5">
+    <box>
+        <div class="label">Columns</div>
+        <input type="number" bind:value={gameData.columns} />
+    </box>
+    <box>
+        <div class="label">Rows</div>
+        <input type="number" bind:value={gameData.rows} />
+    </box>
+    <button class="primary-button" on:click={createGame}>Start game</button>
+</game-settings>
+
+<style lang="postcss">
+    game-settings {
+        @apply w-[300px];
+        @apply flex justify-center items-center gap-4;
+
+        background-color: white;
+        border: lightgrey;
+
+        box {
+            width: 100px;
+        }
+    }
+</style>
---
+++ src/routes/(other)/api/create-game/+server.ts
@@ -0,0 +1,40 @@
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { gameController } from '$lib/server/games/games.controller';
+import { Scoring } from '$lib/game/enums';
+import type { GameSettings } from '$lib/game/types';
+
+export const POST: RequestHandler = (async ({ request }) => {
+    const { columns, rows } = await request.json();
+    const settings: GameSettings = {
+        width: columns,
+        height: rows,
+        initialTime: 5,
+        increment: 3,
+        komi: 3,
+        scoring: Scoring.Area,
+    };
+
+    if (isNaN(settings.width) || settings.width < 1 || isNaN(settings.height) || settings.height < 1) {
+        return error(400, 'Dimension muss eine positive Zahl sein.');
+    }
+    if (isNaN(settings.initialTime) || settings.initialTime <= 0) {
+        return error(400, 'Zeit muss eine positive Zahl sein.');
+    }
+    if (isNaN(settings.increment) || settings.increment < 0) {
+        return error(400, 'Inkrement muss eine positive Zahl sein.');
+    }
+    if (isNaN(settings.komi) || settings.komi < 0) {
+        return error(400, 'Komi muss eine positive Zahl sein.');
+    }
+    if (settings.scoring !== Scoring.Area && settings.scoring !== Scoring.Territory) {
+        return error(400, 'Invalid scoring type');
+    }
+
+    const game = await gameController.create(settings);
+    if (game) {
+        return json({ gameId: game._id });
+    } else {
+        return error(400, 'Komi muss eine positive Zahl sein.');
+    }
+});

53ca3b7829964a6671a6f6564b3d5c9ba434b31f
Mark Nardi <nardimark7@gmail.com>
2024-01-03 11:41:50 +0100

    add email provider

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,33 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GoogleProvider from '@auth/core/providers/google';
+import EmailProvider from '@auth/core/providers/email';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import {
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
     PRIVATE_DB_NAME,
-    AUTH_SECRET
+    AUTH_SECRET,
+    PRIVATE_EMAIL_SERVER_HOST,
+    PRIVATE_EMAIL_SERVER_PORT,
+    PRIVATE_EMAIL_SERVER_USER,
+    PRIVATE_EMAIL_SERVER_PASSWORD,
+    PRIVATE_EMAIL_FROM
 } from '$env/static/private';
 import { mongoClient } from '$lib/server/db';
 
 export const handle = SvelteKitAuth({
     providers: [
+        EmailProvider({
+            server: {
+                host: PRIVATE_EMAIL_SERVER_HOST,
+                port: PRIVATE_EMAIL_SERVER_PORT,
+                auth: {
+                    user: PRIVATE_EMAIL_SERVER_USER,
+                    pass: PRIVATE_EMAIL_SERVER_PASSWORD,
+                },
+            },
+            from: PRIVATE_EMAIL_FROM,
+        }),
         GoogleProvider({
             clientId: GOOGLE_CLIENT_ID,
             clientSecret: GOOGLE_CLIENT_SECRET,

71424333b8795b8c7e5ca6319f8fddb810bf493c
Mark Nardi <nardimark7@gmail.com>
2024-01-06 15:29:10 +0100

    create session on home screen

---
+++ src/lib/components/CreateSession.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import { Button } from '$lib/components/ui/button';
+    import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '$lib/components/ui/card';
+    import { ToggleGroupItem } from '$lib/components/ui/toggle-group';
+    import ToggleGroup from '$lib/components/ui/toggle-group/toggle-group.svelte';
+    import Separator from '$lib/components/ui/separator/separator.svelte';
+
+</script>
+
+<Card>
+	<CardHeader>
+		<CardTitle>How do you want to play?</CardTitle>
+		<CardDescription>
+			Specify board size and time settings.
+		</CardDescription>
+	</CardHeader>
+	<CardContent class="grid gap-6">
+        <ToggleGroup variant="outline" type="multiple" class="grid grid-cols-3 gap-4">
+            <ToggleGroupItem value="9">
+                <h3 class="text-lg font-bold">9x9</h3>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="13">
+                <h3 class="text-lg font-bold">13x13</h3>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="19">
+                <h3 class="text-lg font-bold">19x19</h3>
+            </ToggleGroupItem>
+        </ToggleGroup>
+        <Separator />
+        <ToggleGroup variant="outline" type="multiple" class="grid grid-cols-3 gap-4">
+            <ToggleGroupItem value="5+5">
+                <h3 class="text-lg font-bold">5+5</h3>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="5+10">
+                <h3 class="text-lg font-bold">5+10</h3>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="5+15">
+                <h3 class="text-lg font-bold">5+15</h3>
+            </ToggleGroupItem>
+           
+            <ToggleGroupItem value="15+5">
+                <h3 class="text-lg font-bold">15+5</h3>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="15+10">
+                <h3 class="text-lg font-bold">15+10</h3>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="15+15">
+                <h3 class="text-lg font-bold">15+15</h3>
+            </ToggleGroupItem>
+           
+            <ToggleGroupItem value="25+5">
+                <h3 class="text-lg font-bold">25+5</h3>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="25+10">
+                <h3 class="text-lg font-bold">25+10</h3>
+            </ToggleGroupItem>
+            <ToggleGroupItem value="25+15">
+                <h3 class="text-lg font-bold">25+15</h3>
+            </ToggleGroupItem>
+        </ToggleGroup>        
+	</CardContent>
+	<CardFooter>
+		<Button class="w-full">Let's GO!</Button>
+	</CardFooter>
+</Card>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
     import { goto } from '$app/navigation';
     import CreateCustomGame from '$lib/components/CreateCustomGame.svelte';
+    import CreateSession from '$lib/components/CreateSession.svelte';
     import { Button } from '$lib/components/ui/button';
     import { activeUser } from '$lib/stores/user.store';
 
 </script>
 
 <home-view>
-    <h1 class="text-5xl font-bold">Welcome Player!</h1>
     {#if $activeUser}
-        <CreateCustomGame />
+        <create-game-section>
+            <CreateSession />
+            <CreateCustomGame />
+        </create-game-section>
     {:else}
+
+        <h1 class="text-5xl font-bold">Welcome Player!</h1>
         <h2 class="text-2xl">You're not logged in. To continue, please first create an account or log in</h2>
         <Button on:click={() => goto('/auth/signin')}>Sign in</Button>
     {/if}
@@ -22,5 +27,9 @@
     home-view {
         @apply w-full h-full;
         @apply flex flex-col justify-center items-center gap-8;
+    }
+
+    create-game-section {
+        @apply flex flex-col justify-center items-center gap-4;
     }
 </style>
